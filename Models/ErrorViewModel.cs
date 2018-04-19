d886d10e9275de123377d7ce33352f761583fcc7
Swaminathan Vetri <svswaminathan@gmail.com>
2018-04-20 00:03:42 +0530

    Inital Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace gab2018_aspnet_core_demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
